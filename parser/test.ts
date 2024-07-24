fe9e8d77e3b1b9722353d732e915ae8836ea4561
Muthu Kumar <muthukumar@thefeathers.in>
2024-07-24 13:32:37 +0530

    test: roundtrip test

--- parser/test.ts
+++ parser/test.ts
@@ -1,4 +1,4 @@
-import { parse } from "./index.ts";
+import { DeclarationFile, parse } from "./index.ts";
 import { assertParser } from "./utils.ts";
 
 import { Comment } from "./comment.ts";
@@ -9,11 +9,22 @@ import { DocString } from "./docString.ts";
 import { Predefined } from "./predefined.ts";
 import { Literal } from "./literal.ts";
 import { Identifier } from "./identifier.ts";
+import { ParserBase } from "./base.ts";
+import { assertThrows } from "jsr:@std/assert@1.0.0/throws";
+import { assert, assertEquals } from "jsr:@std/assert@1.0.0";
+import { assertParserFn } from "./utils.ts";
 
-Deno.test("parse", () => {
-	assertParser(
-		parse,
-		`
+Deno.test("ParserBase", () => {
+	assertThrows(() => {
+		ParserBase.from();
+	});
+
+	assertThrows(() => {
+		ParserBase.parse.run("test");
+	});
+});
+
+const testSource = `
 /// Extract from lib.dom.d.ts
 /// <reference path="./iterable.d.ts" />
 
@@ -23,10 +34,16 @@ Deno.test("parse", () => {
 /// Window APIs
 /////////////////////////////
 
+interface A extends B, C, D {
+	foo?: Bar;
+	baz: string | number;
+}
+
 interface AddEventListenerOptions extends EventListenerOptions {
 	once?: boolean;
 	passive?: boolean;
 	signal?: AbortSignal;
+	init?: string[][];
 }
 
 interface ComputedKeyframe {
@@ -35,7 +52,6 @@ interface ComputedKeyframe {
 	easing: string;
 	offset: number | null;
 	[property: string]: string | number | null | undefined;
-	init?: string[][];
 }
 	
 interface KeyboardEventInit extends EventModifierInit {
@@ -48,75 +64,143 @@ interface KeyboardEventInit extends EventModifierInit {
 	keyCode?: number;
 	location?: number;
 	repeat?: boolean;
-}`,
+}`;
+
+const expectFixture = [
+	Comment.single("/ Extract from lib.dom.d.ts"),
+	Reference.from("./iterable.d.ts"),
+	Comment.multi(" Source is from @types/web "),
+	Comment.single("///////////////////////////"),
+	Comment.single("/ Window APIs"),
+	Comment.single("///////////////////////////"),
+
+	InterfaceDeclaration.from(
+		"A",
 		[
-			Comment.single("/ Extract from lib.dom.d.ts"),
-			Reference.from("./iterable.d.ts"),
-			Comment.multi(" Source is from @types/web "),
-			Comment.single("///////////////////////////"),
-			Comment.single("/ Window APIs"),
-			Comment.single("///////////////////////////"),
-
-			InterfaceDeclaration.from(
-				"AddEventListenerOptions",
-				[
-					Member.from(Identifier.from("once"), Predefined.Boolean.from(), { optional: true }),
-					Member.from(Identifier.from("passive"), Predefined.Boolean.from(), { optional: true }),
-					Member.from(Identifier.from("signal"), TypeReference.from(Identifier.from("AbortSignal")), {
-						optional: true,
-					}),
-				],
-				{
-					extends: [TypeReference.from(Identifier.from("EventListenerOptions"))],
-				},
-			),
-
-			InterfaceDeclaration.from("ComputedKeyframe", [
-				Member.from(
-					Identifier.from("composite"),
-					TypeReference.from(Identifier.from("CompositeOperationOrAuto"), null),
-				),
-				Member.from(Identifier.from("computedOffset"), Predefined.Number.from()),
-				Member.from(Identifier.from("easing"), Predefined.String.from()),
-				Member.from(Identifier.from("offset"), UnionType.from([Predefined.Number.from(), Literal.Null.from()])),
-				Member.from(
-					IndexKey.from("property", Predefined.String.from()),
-					UnionType.from([
-						Predefined.String.from(),
-						UnionType.from([
-							Predefined.Number.from(),
-							UnionType.from([Literal.Null.from(), Literal.Undefined.from()]),
-						]),
-					]),
-				),
-				Member.from(Identifier.from("init"), ArrayType.from(ArrayType.from(Predefined.String.from())), {
-					optional: true,
-				}),
-			]),
-
-			InterfaceDeclaration.from(
-				"KeyboardEventInit",
-				[
-					Member.from(Identifier.from("charCode"), Predefined.Number.from(), {
-						optional: true,
-						doc: DocString.from(" @deprecated "),
-					}),
-					Member.from(Identifier.from("code"), Predefined.String.from(), { optional: true }),
-					Member.from(Identifier.from("isComposing"), Predefined.Boolean.from(), {
-						optional: true,
-					}),
-					Member.from(Identifier.from("key"), Predefined.String.from(), { optional: true }),
-					Member.from(Identifier.from("keyCode"), Predefined.Number.from(), {
-						optional: true,
-						doc: DocString.from(" @deprecated "),
-					}),
-					Member.from(Identifier.from("location"), Predefined.Number.from(), { optional: true }),
-					Member.from(Identifier.from("repeat"), Predefined.Boolean.from(), { optional: true }),
-				],
-				{
-					extends: [TypeReference.from(Identifier.from("EventModifierInit"))],
-				},
+			Member.from(Identifier.from("foo"), TypeReference.from(Identifier.from("Bar")), { optional: true }),
+			Member.from(
+				Identifier.from("baz"), //
+				UnionType.from([Predefined.String.from(), Predefined.Number.from()]),
 			),
 		],
-	);
+		{
+			extends: [
+				TypeReference.from(Identifier.from("B")),
+				TypeReference.from(Identifier.from("C")),
+				TypeReference.from(Identifier.from("D")),
+			],
+		},
+	),
+
+	InterfaceDeclaration.from(
+		"AddEventListenerOptions",
+		[
+			Member.from(Identifier.from("once"), Predefined.Boolean.from(), { optional: true }),
+			Member.from(Identifier.from("passive"), Predefined.Boolean.from(), { optional: true }),
+			Member.from(Identifier.from("signal"), TypeReference.from(Identifier.from("AbortSignal")), {
+				optional: true,
+			}),
+			Member.from(
+				Identifier.from("init"), //
+				ArrayType.from(ArrayType.from(Predefined.String.from())),
+				{ optional: true },
+			),
+		],
+		{
+			extends: [TypeReference.from(Identifier.from("EventListenerOptions"))],
+		},
+	),
+
+	InterfaceDeclaration.from("ComputedKeyframe", [
+		Member.from(
+			Identifier.from("composite"),
+			TypeReference.from(Identifier.from("CompositeOperationOrAuto"), null),
+		),
+		Member.from(
+			Identifier.from("computedOffset"), //
+			Predefined.Number.from(),
+		),
+		Member.from(
+			Identifier.from("easing"), //
+			Predefined.String.from(),
+		),
+		Member.from(
+			Identifier.from("offset"), //
+			UnionType.from([Predefined.Number.from(), Literal.Null.from()]),
+		),
+		Member.from(
+			IndexKey.from("property", Predefined.String.from()),
+			UnionType.from([
+				Predefined.String.from(),
+				UnionType.from([
+					Predefined.Number.from(),
+					UnionType.from([Literal.Null.from(), Literal.Undefined.from()]),
+				]),
+			]),
+		),
+	]),
+
+	InterfaceDeclaration.from(
+		"KeyboardEventInit",
+		[
+			Member.from(
+				Identifier.from("charCode"), //
+				Predefined.Number.from(),
+				{ optional: true, doc: DocString.from(" @deprecated ") },
+			),
+			Member.from(
+				Identifier.from("code"), //
+				Predefined.String.from(),
+				{ optional: true },
+			),
+			Member.from(
+				Identifier.from("isComposing"), //
+				Predefined.Boolean.from(),
+				{ optional: true },
+			),
+			Member.from(
+				Identifier.from("key"), //
+				Predefined.String.from(),
+				{ optional: true },
+			),
+			Member.from(
+				Identifier.from("keyCode"), //
+				Predefined.Number.from(),
+				{ optional: true, doc: DocString.from(" @deprecated ") },
+			),
+			Member.from(
+				Identifier.from("location"), //
+				Predefined.Number.from(),
+				{ optional: true },
+			),
+			Member.from(
+				Identifier.from("repeat"), //
+				Predefined.Boolean.from(),
+				{ optional: true },
+			),
+		],
+		{
+			extends: [TypeReference.from(Identifier.from("EventModifierInit"))],
+		},
+	),
+];
+
+Deno.test("DeclarationFile", () => {
+	assertParser(DeclarationFile.parse, testSource, DeclarationFile.from(expectFixture));
+});
+
+Deno.test("parse", () => {
+	assertParserFn(parse, testSource, DeclarationFile.from(expectFixture));
+});
+
+Deno.test("roundtrip", () => {
+	const parsed = parse(testSource);
+	assert(!parsed.isError);
+	const printed = parsed.result.toString();
+	const reparsed = parse(printed);
+	assert(!reparsed.isError);
+
+	assertEquals(reparsed.result, parsed.result);
+	assertEquals(reparsed.result, DeclarationFile.from(expectFixture));
+	assertEquals(reparsed.result.toString(), parsed.result.toString());
 });
