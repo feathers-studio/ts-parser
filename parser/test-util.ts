96649ff78dbf1065e5442939f021ab067a8444ab
Muthu Kumar <muthukumar@thefeathers.in>
2024-07-31 19:23:46 +0530

    feat: better logging in test-util

--- parser/test-util.ts
+++ parser/test-util.ts
@@ -1,6 +1,36 @@
-import { Parser } from "arcsecond";
-import { assert, assertEquals } from "@std/assert";
-import { ends } from "./utils.ts";
+import { Parser, ResultType } from "arcsecond";
+import { ends, last } from "./utils.ts";
+import { expect } from "bun:test";
+
+const printErrorSrc = (
+	direction: "Forwards" | "Inverse",
+	source: string,
+	result: ResultType<unknown, string, string>,
+) => {
+	if (!result.isError) return;
+
+	const index = result.index;
+
+	const sourceBits = source
+		.slice(0, index + 1) // +1 to include the error character
+		.replace(/\t/g, "    ")
+		.split("\n") // Split by lines
+		.slice(-5); // Take upto the last 5 lines
+
+	const afterError = source.slice(index + 1).split("\n")[0];
+
+	const leading = last(sourceBits).length - 1;
+	const errLine = "\n" + " ".repeat(leading) + "^";
+	const errorSrc = sourceBits.join("\n") + afterError + errLine;
+
+	console.log("\n");
+	console.error(`-- ${direction} -- Parsing source errored here:\n`);
+	console.error(errorSrc);
+	console.error(result.error);
+	console.log("\n");
+
+	throw 1;
+};
 
 export const assertParser = <T>(
 	parser: Parser<T>,
@@ -20,13 +50,18 @@ export const assertParser = <T>(
 
 	{
 		const result = ended.run(source);
-		if (requireFail) return assert(result.isError, " (Forwards)");
-		else
-			assertEquals(
-				result,
-				{ isError: false, result: expected, index: source.length, data: null },
-				" *<Forwards>",
-			);
+		if (requireFail) return expect(result.isError).toBeTrue();
+		else {
+			if (result.isError) printErrorSrc("Forwards", source, result);
+			else {
+				expect(result, "-- Forwards --").toEqual({
+					isError: false,
+					result: expected,
+					index: source.length,
+					data: null,
+				});
+			}
+		}
 	}
 
 	if (skipInverse) return;
@@ -36,25 +71,18 @@ export const assertParser = <T>(
 		const newSource = String(expected);
 		const result2 = ended.run(newSource);
 
-		let errorPos = result2.isError
-			? "\n" +
-			  newSource
-					.slice(result2.index - 60, result2.index + 1)
-					.split("\n")
-					.at(-1)
-			: "";
-
-		errorPos += errorPos ? "\n" + " ".repeat(errorPos.length) + "^" : "";
-
-		assertEquals(
-			result2,
-			{ isError: false, result: expected, index: newSource.length, data: null },
-			" *<Backwards>" + errorPos,
-		);
+		if (result2.isError) printErrorSrc("Inverse", newSource, result2);
+		else
+			expect(result2, "-- Inverse --").toEqual({
+				isError: false,
+				result: expected,
+				index: newSource.length,
+				data: null,
+			});
 	}
 };
 
 export const assertParserFn = <T>(parserFn: Parser<T>["run"], source: string, expected: T) => {
 	const result = parserFn(source);
-	assertEquals(result, { isError: false, result: expected, index: source.length, data: null });
+	expect(result).toEqual({ isError: false, result: expected, index: source.length, data: null });
 };
